ebe1f37b80c660d1b787caec5a56db24497a5381
auxten <auxtenwpc@gmail.com>
2021-02-01 17:56:56 +0800

    Add test file

---
+++ test/atmj/gen_test_data.py
@@ -0,0 +1,48 @@
+#!python
+# coding=utf-8
+
+import json
+import random
+import time
+
+TRANS_BRAN_CODE_LIST = (
+    "11670101", "11670102", "11670103", "11670104", "11670105", "11670106", "11670107", "11670108", "11670109",
+    "11670110"
+)
+
+ATMC_TRS_CODE_LIST = ("INQ", "LIS", "CWD", "CDP", "TFR", "PIN", "REP", "PAY", "XXX", "YYY", "ZZZ")
+
+start_time = 946656000  # 2000-01-01 00:00:00
+
+with open("./atmj_msg_3000000_test.txt", "w") as data:
+    for i in range(3000000):
+        # 步长 1min 增长
+        cur_time = start_time + i * 60
+        body_insert = {
+            "after": {
+                "TANS_AMT": "100.01",
+                "TRANS_FLAG": "P",
+                "TRANS_DATE": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(cur_time)),
+                "TRANS_BRAN_CODE": random.choice(TRANS_BRAN_CODE_LIST),
+                "ATMC_TRSCODE": random.choice(ATMC_TRS_CODE_LIST),
+            },
+            "rowid": str(i),
+            "scntime": cur_time,
+            "optype": "INSERT",
+            "name": "atmj",
+        }
+
+        body_update = {
+            "rowid": str(i),
+            "scntime": cur_time + 1,
+            "optype": "UPDATE",
+            "name": "atmj",
+            "after": {
+                "TRANS_FLAG": "0"
+            },
+            "before": {
+                "TRANS_FLAG": "p"
+            }
+        }
+        data.write(json.dumps(body_insert) + "\n")
+        data.write(json.dumps(body_update) + "\n")
