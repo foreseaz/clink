8244c1ed0d8eb6f5308f3bbb46fb1edcda3b0027
auxten <auxtenwpc@gmail.com>
2021-02-13 20:32:18 +0800

    Add ngncol

---
+++ ngncol/engine_test.go
@@ -0,0 +1,64 @@
+package ngncol
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/auxten/clink/ngncol/schema"
+)
+
+func TestEngine(t *testing.T) {
+	Convey("New engine", t, func() {
+		s, err := schema.LoadConf("../test/atmj/schema_test.yaml")
+		So(err, ShouldBeNil)
+
+		eng := NewEngine("atmj", s)
+		So(err, ShouldBeNil)
+		err = eng.InitTables()
+		So(err, ShouldBeNil)
+		schemaStr, err := eng.ShowSchema()
+		So(err, ShouldBeNil)
+		So(schemaStr, ShouldResemble,
+			`CREATE TABLE 'atmj' (
+'rowid' string PRIMARY KEY NOT NULL,
+'scntime' bigint,
+'TANS_AMT' bigint DEFAULT 0,
+'TRANS_FLAG' string,
+'TRANS_DATE' date,
+'TRANS_BRAN_CODE' string,
+'ATMC_TRSCODE' string
+)`)
+		indexStr, err := eng.ShowIndex()
+		So(err, ShouldBeNil)
+		So(indexStr, ShouldResemble, "CREATE INDEX 'idx__atmj__TRANS_FLAG' ON `atmj` (`TRANS_FLAG`);\nCREATE INDEX 'idx__atmj__TRANS_DATE' ON `atmj` (`TRANS_DATE`);\n")
+		f, err := os.Open("../test/atmj/atmj_msg_1000_test.txt")
+		So(err, ShouldBeNil)
+		sc := bufio.NewScanner(f)
+		for sc.Scan() {
+			err = eng.Exec("atmj", sc.Bytes())
+			So(err, ShouldBeNil)
+		}
+		rows, err := eng.Db.Query(eng.Schema.Query)
+		So(err, ShouldBeNil)
+		defer rows.Close()
+		var (
+			transDate     string
+			transBranCode string
+			balance       float64
+			count         int64
+		)
+
+		for rows.Next() {
+			err := rows.Scan(&transDate, &transBranCode, &balance, &count)
+			So(err, ShouldBeNil)
+			fmt.Printf("TRANS_DATE %s\n", transDate)
+			fmt.Printf("TRANS_BRAN_CODE %s\n", transBranCode)
+			fmt.Printf("BALANCE %f\n", balance)
+			fmt.Printf("CNT %d\n", count)
+		}
+	})
+}
---
+++ ngncol/schema/msg2sql_test.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMsgToSQL(t *testing.T) {
+	Convey("msg to sql", t, func() {
+		s, err := LoadConf("../test/atmj/schema_test.yaml")
+		So(err, ShouldBeNil)
+		msg := &Msg{
+			Value: []byte(`{"after":{"TANS_AMT":"100.01","TRANS_FLAG":"P","TRANS_DATE":"2001-03-08 23:21:00",
+				"TRANS_BRAN_CODE":"11670103","ATMC_TRSCODE":"CWD"},"rowid":"623481","scntime":984064860,"optype":"INSERT","name":"ATMJ_JOUR"}`),
+			Table: &s.Tables[0],
+		}
+
+		So(msg.ToSQL(), ShouldResemble, "INSERT INTO atmj (`rowid`,`scntime`,`TANS_AMT`,`TRANS_FLAG`,`TRANS_DATE`,`TRANS_BRAN_CODE`,`ATMC_TRSCODE`) VALUES ('623481',984064860,100.01,'P','2001-03-08 23:21:00','11670103','CWD');")
+
+		msg.Value = []byte(`{"rowid":"623481","scntime":984064861,"optype":"UPDATE",
+			"name":"ATMJ_JOUR","after":{"TRANS_FLAG":"0"},"before":{"TRANS_FLAG":"p"}}`)
+
+		So(msg.ToSQL(), ShouldResemble, "UPDATE atmj SET `scntime` = 984064861, `TRANS_FLAG` = '0' WHERE `rowid` = '623481';")
+	})
+}
