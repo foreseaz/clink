8fe2e87432e04df8c97b7ae17105e5ed85cca3da
auxten <auxtenwpc@gmail.com>
2021-03-22 19:34:16 +0800

    Add MySQLSrc, KafkaSrc

--- core/schema.go
+++ core/schema.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"io/ioutil"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
@@ -15,20 +16,56 @@ type Schema struct {
 	TableMap map[string]*Table `yaml:"-"` // tableName:table
 }
 
-type Table struct {
-	Name        string
-	Topic       string
+type DataSource interface{}
+
+type KafkaSrc struct {
+	DataSource `yaml:"-"`
+	// EndPointConfig in json format
+	EndPointConfig string
+	// OptTypePath is the json path of Operation type.
+	// eg. INSERT/UPDATE
 	OptTypePath string
-	Pk          string
-	Cols        []Col
-	Index       []string
+	// Topic is the kafka topic name, one topic per table
+	Topic string
+}
+
+type MySQLSrc struct {
+	DataSource `yaml:"-"`
+	// Dsn is the connection string of DB
+	// eg. `username:password@protocol(address)/dbname?param=value`
+	Dsn string
+	// Table name or a Select query return a table
+	Table  string
+	Select string
+	// TTL is the cache time of the table
+	TTL time.Duration
+}
+
+// Table is the description of table in ngnx.
+type Table struct {
+	Name string
+	// Type is the type of table, eg. kafka/mysql
+	Type string
+	// DataSource is the data source description
+	DataSource DataSource
+	// Pk is the primary key name.
+	Pk string
+	// Cols are the columns in the Table
+	Cols []Col
+	// Index is the column names array to build an index
+	Index []string
 }
 
 type Col struct {
-	Name       string
-	Type       string
-	Extra      string
+	// Name is the column name
+	Name string
+	// Type is the column schema type
+	Type string
+	// Extra column description, eg. UNIQUE/DEFAULT
+	Extra string
+	// InsertPath is the json path of INSERT column data
 	InsertPath string
+	// UpdatePath is the json path of UPDATE column data
 	UpdatePath string
 }
 
--- core/schema_test.go
+++ core/schema_test.go
@@ -1,8 +1,10 @@
 package core
 
 import (
+	"bytes"
 	"io/ioutil"
 	"testing"
+	"time"
 
 	. "github.com/smartystreets/goconvey/convey"
 	"gopkg.in/yaml.v3"
@@ -21,10 +23,14 @@ AND t1.TRANS_FLAG = '0'
 GROUP BY SUBSTRING(t1.TRANS_DATE, 0, 10),t1.TRANS_BRAN_CODE`,
 	Tables: []Table{
 		{
-			Name:        "mj",
-			Topic:       "mj",
-			OptTypePath: "optype",
-			Pk:          "rowid",
+			Name: "mj",
+			Type: "kafka",
+			DataSource: KafkaSrc{
+				EndPointConfig: `{"bootstrap.servers": "192.168.10.90:9092,192.168.10.90:9093,192.168.10.90:9094", "broker.address.family": "v4",}`,
+				Topic:          "mj",
+				OptTypePath:    "optype",
+			},
+			Pk: "rowid",
 			Cols: []Col{
 				{
 					Name:       "rowid",
@@ -83,6 +89,136 @@ GROUP BY SUBSTRING(t1.TRANS_DATE, 0, 10),t1.TRANS_BRAN_CODE`,
 	},
 }
 
+var confHybrid = &Schema{
+	Name:   "schema",
+	Engine: "compact+:memory:",
+	Query: `SELECT SUBSTRING(t1.TRANS_DATE, 0, 10) as trans_date,
+t1.TRANS_BRAN_CODE as trans_bran_code,
+ROUND(SUM(t1.TANS_AMT)/10000,2) as balance,
+count(t1.rowid) as cnt
+FROM mj t1
+WHERE t1.MC_TRSCODE in ('INQ', 'LIS', 'CWD', 'CDP', 'TFR', 'PIN', 'REP', 'PAY')
+AND t1.TRANS_FLAG = '0'
+GROUP BY SUBSTRING(t1.TRANS_DATE, 0, 10),t1.TRANS_BRAN_CODE`,
+	Tables: []Table{
+		{
+			Name: "mj",
+			Type: "kafka",
+			DataSource: KafkaSrc{
+				EndPointConfig: `{"bootstrap.servers": "192.168.10.90:9092,192.168.10.90:9093,192.168.10.90:9094", "broker.address.family": "v4",}`,
+				Topic:          "mj",
+				OptTypePath:    "optype",
+			},
+			Pk: "rowid",
+			Cols: []Col{
+				{
+					Name:       "rowid",
+					Type:       "string",
+					Extra:      "NOT NULL",
+					InsertPath: "rowid",
+					UpdatePath: "rowid",
+				},
+				{
+					Name:       "scntime",
+					Type:       "bigint",
+					Extra:      "",
+					InsertPath: "scntime",
+					UpdatePath: "scntime",
+				},
+				{
+					Name:       "TANS_AMT",
+					Type:       "bigint",
+					Extra:      "DEFAULT 0",
+					InsertPath: "after.TANS_AMT",
+					UpdatePath: "after.TANS_AMT",
+				},
+				{
+					Name:       "TRANS_FLAG",
+					Type:       "string",
+					Extra:      "",
+					InsertPath: "after.TRANS_FLAG",
+					UpdatePath: "after.TRANS_FLAG",
+				},
+				{
+					Name:       "TRANS_DATE",
+					Type:       "date",
+					Extra:      "",
+					InsertPath: "after.TRANS_DATE",
+					UpdatePath: "after.TRANS_DATE",
+				},
+				{
+					Name:       "TRANS_BRAN_CODE",
+					Type:       "string",
+					Extra:      "",
+					InsertPath: "after.TRANS_BRAN_CODE",
+					UpdatePath: "after.TRANS_BRAN_CODE",
+				},
+				{
+					Name:       "MC_TRSCODE",
+					Type:       "string",
+					Extra:      "",
+					InsertPath: "after.MC_TRSCODE",
+					UpdatePath: "after.MC_TRSCODE",
+				},
+			},
+			Index: []string{
+				"TRANS_FLAG", "TRANS_DATE", "TRANS_BRAN_CODE",
+			},
+		},
+		{
+			Name: "mysql_src1",
+			Type: "mysql",
+			DataSource: MySQLSrc{
+				Dsn:    "username:password@protocol(address)/dbname?param=value",
+				Table:  "test_table",
+				Select: "",
+				TTL:    time.Hour,
+			},
+			Pk: "primary",
+			Cols: []Col{
+				{
+					Name: "primary",
+					Type: "bigint",
+				},
+				{
+					Name: "idx_col",
+					Type: "int",
+				},
+				{
+					Name: "income",
+					Type: "int",
+				},
+			},
+			Index: []string{"idx_col"},
+		},
+		{
+			Name: "mysql_src2",
+			Type: "mysql",
+			DataSource: MySQLSrc{
+				Dsn:    "username:password@protocol(address)/dbname2?param=value",
+				Select: "SELECT primary2, idx_col2, income2 FROM mysql_src2 ORDER BY income2 DESC LIMIT 1",
+				TTL:    2 * time.Hour,
+			},
+			Pk: "primary2",
+			Cols: []Col{
+				{
+					Name: "primary2",
+					Type: "bigint",
+				},
+				{
+					Name: "idx_col2",
+					Type: "int",
+				},
+				{
+					Name: "income2",
+					Type: "int",
+				},
+			},
+			Index: []string{"idx_col2"},
+		},
+	},
+}
+
 func TestSchema(t *testing.T) {
 	/*
 		INSERT:
@@ -119,7 +255,11 @@ func TestSchema(t *testing.T) {
 		confByteFromFile, err2 := ioutil.ReadFile("../test/mj/schema_test.yaml")
 		So(err, ShouldBeNil)
 		So(err2, ShouldBeNil)
-		So(string(confByte), ShouldResemble, string(confByteFromFile))
+		So(
+			string(bytes.ReplaceAll(confByte, []byte("  "), []byte(""))),
+			ShouldResemble,
+			string(bytes.ReplaceAll(confByteFromFile, []byte("  "), []byte(""))),
+		)
 	})
 	Convey("Load schema config", t, func() {
 		schema, err := LoadConf("../test/mj/schema_test.yaml")
